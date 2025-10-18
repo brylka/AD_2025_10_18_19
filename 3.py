1117fba483ac7cdbfb3496d3d96622c03cb23b79
Bartosz Bryniarski <brylka@jelenia.com>
2025-10-18 11:08:41 +0200

    Sobota

--- 3.py
+++ 3.py
@@ -1,7 +1,11 @@
-from sklearn.metrics import accuracy_score, classification_report
+from types import new_class
+
+from sklearn.metrics import accuracy_score, classification_report, roc_curve, auc
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
 
 # Załadowanie danych
 data = load_iris()
@@ -20,3 +24,20 @@ y_proba_gnb = gnb_classifier.predict_proba(X_test)
 print(f"Dokładność Gaussian Naive Bayes: {accuracy_score(y_test, y_pred_gnb)}")
 print('Raport klasyfikacji:')
 print(classification_report(y_test, y_pred_gnb, target_names=data.target_names))
+
+y_test_bin = label_binarize(y_test, classes=[0, 1, 2])
+n_classes = y_test_bin.shape[1]
+
+plt.figure(figsize=(10, 8))
+for i in range(n_classes):
+    fpr, tpr, _ = roc_curve(y_test_bin[:, i], y_proba_gnb[:, i])
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, label=f"{data.target_names[i]} (AUC = {roc_auc}")
+
+plt.plot([0, 1], [0, 1], 'k--', label="Losowy klasyfikator")
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Krzywa ROC - Naive Bayes')
+plt.legend()
+plt.grid(True)
+plt.show()
